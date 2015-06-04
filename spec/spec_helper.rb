cdc6efca0c98e467ab0613602380cd0a153aeded
Maskim Burnin <Maksim.Burnin@gmail.com>
2015-06-04 10:43:59 +0500

    spec/config.yml is not needed anymore, since we don't send real requests anymore

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,3 @@ require 'amocrm'
 SPEC_ROOT = Pathname.new File.dirname(__FILE__)
 
 Dir[SPEC_ROOT.join("support/**/*.rb")].each { |f| require f }
-
-# AmoCRM API config file
-
-config_file = SPEC_ROOT.join "config.yml"
-raise 'spec/config.yml not found, please create one' unless File.exist? config_file
-CONFIG = YAML.load_file config_file
